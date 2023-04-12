38e8039a24cc51476b4cf0da2b153d82f410f48e
Mbykz01 <aminuabdulkarimibrahimu@gmail.com>
2023-04-11 20:28:50 -0700

    insert_school.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on keyword arguments.
+    Returns the new _id.
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
